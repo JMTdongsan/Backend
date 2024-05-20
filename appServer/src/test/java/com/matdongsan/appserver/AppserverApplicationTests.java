b052e37cd34880e39a9bb36298c037213d2c5246
GwonPyo <ngp9441@gmail.com>
2024-05-20 16:18:50 +0900

    add appServer's initial files

---
+++ appServer/src/test/java/com/matdongsan/appserver/AppserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matdongsan.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
