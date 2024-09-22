cdc00fe3bc85f12827804dc895d037e6be096bad
GwonPyo <ngp9441@gmail.com>
2024-09-23 08:23:18 +0900

    요청 로그 필터 추가

---
+++ src/main/java/com/matdongsan/demo/configs/webconfig/LogFilter.java
@@ -0,0 +1,31 @@
+package com.matdongsan.demo.configs.webconfig;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        log.info("Request: {}", requestURI);
+        chain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/matdongsan/demo/configs/webconfig/WebConfig.java
@@ -0,0 +1,20 @@
+package com.matdongsan.demo.configs.webconfig;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
