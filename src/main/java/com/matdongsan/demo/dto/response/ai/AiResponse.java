6fb96b7582ebb7ab701d14a70c3801849cfeeed7
GwonPyo <ngp9441@gmail.com>
2024-09-30 00:06:51 +0900

    챗봇 응답을 받기 위한 클래스 추가.

---
+++ src/main/java/com/matdongsan/demo/dto/response/ai/AiResponse.java
@@ -0,0 +1,14 @@
+package com.matdongsan.demo.dto.response.ai;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiResponse {
+
+    private String answer;
+    private String question;
+}
