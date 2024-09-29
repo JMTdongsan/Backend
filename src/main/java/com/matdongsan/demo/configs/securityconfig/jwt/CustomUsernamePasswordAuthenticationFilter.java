fc827ec3bfee5ca0665ab82464880a9f532ae553
GwonPyo <ngp9441@gmail.com>
2024-09-29 19:46:23 +0900

    유저 로그인 요청 시 json으로도 가능하게 수정 (기존에는 form-date 형식만 허용)

--- src/main/java/com/matdongsan/demo/configs/securityconfig/jwt/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/matdongsan/demo/configs/securityconfig/jwt/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,21 +1,28 @@
 package com.matdongsan.demo.configs.securityconfig.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.matdongsan.demo.dto.request.member.LoginRequest;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Iterator;
 
+@Slf4j
 @RequiredArgsConstructor
 public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -24,11 +31,33 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
 
+    private ObjectMapper objectMapper =  new ObjectMapper();
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
 
         String username = obtainUsername(request);
         String password = obtainPassword(request);
+
+        if (username == null & password == null) {
+            LoginRequest loginRequest = new LoginRequest();
+
+            ServletInputStream inputStream = null;
+            try {
+                inputStream = request.getInputStream();
+                String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+                loginRequest = objectMapper.readValue(messageBody, LoginRequest.class);
+
+            } catch (IOException e) {
+                throw new RuntimeException("유효하지 않은 접근입니다. 데이터 형식을 다시 확인해주세요.");
+            }
+
+            username = loginRequest.getUsername();
+            password = loginRequest.getPassword();
+        }
+
+        log.info("username: {} - 로그인 진행", username);
 
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password, null);
 
@@ -57,6 +86,4 @@ public class CustomUsernamePasswordAuthenticationFilter extends UsernamePassword
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
         response.setStatus(401);
     }
-
-
 }
