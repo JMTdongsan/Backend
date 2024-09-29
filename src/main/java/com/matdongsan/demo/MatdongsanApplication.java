5d809e66dc4fae707852577b2140d465cc53ccda
GwonPyo <ngp9441@gmail.com>
2024-09-29 19:47:38 +0900

    React와 연결할 때 Cors문제가 발생하여 관련 Config 추가.

--- src/main/java/com/matdongsan/demo/MatdongsanApplication.java
+++ src/main/java/com/matdongsan/demo/MatdongsanApplication.java
@@ -3,9 +3,12 @@ package com.matdongsan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MatdongsanApplication {
@@ -14,4 +17,16 @@ public class MatdongsanApplication {
 		SpringApplication.run(MatdongsanApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.maxAge(3000);
+            }
+        };
+    }
 }
