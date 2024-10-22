cefda41cf10e9ff01a40f46faf6874e9cd6a7e2e
GwonPyo <ngp9441@gmail.com>
2024-10-23 03:42:23 +0900

    유저 엔티티에 선호 기사 관련 임베딩 추가 + 임베딩 반환 및 설정 메서드 추가

--- src/main/java/com/matdongsan/demo/mysql/domain/Member.java
+++ src/main/java/com/matdongsan/demo/mysql/domain/Member.java
@@ -1,10 +1,13 @@
 package com.matdongsan.demo.mysql.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.lang.NonNull;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,8 +40,35 @@ public class Member {
     @Column(name = "email")
     private String email;
 
+    @Column(name = "preferenceVector", columnDefinition = "TEXT")
+    private String preferenceVector;
+
     @JsonIgnore
     @Builder.Default
     @OneToMany(mappedBy = "creator")
     private List<ChatRoom> chatRooms = new ArrayList<>();
+
+    public List<Float> getPreferenceVectorList() {
+        List<Float> preferenceVectorList = new ArrayList<>();
+        String[] values = preferenceVector.split(",");
+
+        for (String value : values) {
+            preferenceVectorList.add(Float.parseFloat(value));
+        }
+
+        return preferenceVectorList;
+    }
+
+    public void setPreferenceVector(List<Float> preferenceVectorList) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < preferenceVectorList.size(); i++) {
+            stringBuilder.append(preferenceVectorList.get(i));
+            if (i < preferenceVectorList.size()-1) {
+               stringBuilder.append(",");
+            }
+        }
+
+        preferenceVector = stringBuilder.toString();
+    }
 }
